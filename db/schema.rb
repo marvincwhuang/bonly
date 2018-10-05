726058e8ea413f5703a091dfa6d2df67c81d5e60
Chi-Wei Huang <cwhuang0523@gmail.com>
2018-10-05 16:23:15 +0800

    only admin can enter admin

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_082521) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
