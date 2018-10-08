ba3ee6e0036a747fed60fa369068795b72ed9aba
Chi-Wei Huang <cwhuang0523@gmail.com>
2018-10-08 14:53:22 +0800

    updated styling

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 ] ]"
+
 module Bonly
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
