cf759540e49e499397eeee1d6d08187091eed248
Chiwei Huang <cwhuang0523@gmail.com>
2018-11-01 21:02:14 +0800

    diable button when uploadding to avoid multi submit

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_104501) do
     t.datetime "updated_at", null: false
     t.string "image_file_name"
     t.string "image_content_type"
-    t.integer "image_file_size"
+    t.bigint "image_file_size"
     t.datetime "image_updated_at"
     t.integer "user_id"
   end
